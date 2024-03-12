0cd59ddaef05cb36923ceaea7fd3d380b697cc8d
Jin young <xmfkdnak98@gmail.com>
2024-03-12 09:59:35 +0900

    index페이지 api로 받아온 데이터 적용

--- js/index.js
+++ js/index.js
@@ -35,17 +35,160 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-fetch("http://localhost/api/boxOfficeApi.php/")
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
 
-fetch("http://localhost/api/showingApi.php/")
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+const boxOfficeApi = async () => {
+  try {
+    return fetch("http://localhost/api/boxOfficeApi.php/")
+    .then(response => response.json())
+    .then(data => {
+      const section = document.querySelector('.one .pop');
+      section.innerHTML = "";
+      for(let i=0;i<5;i++){
+        const popShow = document.createElement('div');
+        popShow.classList.add('pop-show');
 
-fetch("http://localhost/api/kidsShowApi.php/")
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+        const showImg = document.createElement('div');
+        showImg.classList.add('show-img');
+        
+        const img = document.createElement('img');
+        img.setAttribute('src', `http://www.kopis.or.kr/${data?.boxof[i]?.poster}`);
+        showImg.appendChild(img);
+
+        const popDescription = document.createElement('div');
+        popDescription.classList.add('pop-description')
+        const ul = document.createElement('ul');
+        
+        const name = document.createElement('li');
+        const bold = document.createElement('strong');
+        bold.innerText = data?.boxof[i].prfnm;
+        name.appendChild(bold);
+        ul.appendChild(name);
+
+        const date = document.createElement('li');
+        date.innerText = data?.boxof[i].prfpd;
+        ul.appendChild(date);
+
+        const genre = document.createElement('li');
+        genre.innerText = data?.boxof[i].cate;
+        ul.appendChild(genre);
+
+        popDescription.appendChild(ul);
+
+
+        popShow.appendChild(showImg);
+        popShow.appendChild(popDescription);
+
+        section.appendChild(popShow);
+      }
+    })
+  }catch(err) {
+    console.log(`Error: ${err}`);
+  }
+}
+
+const nowSowing = async () => {
+  try {
+    return fetch("http://localhost/api/showingApi.php/")
+    .then(response => response.json())
+    .then(data => {
+      const section = document.querySelector('.two .pop');
+      section.innerHTML = "";
+      data.db.forEach(item => {
+        const popShow = document.createElement('div');
+        popShow.classList.add('pop-show');
+
+        const showImg = document.createElement('div');
+        showImg.classList.add('show-img');
+        
+        const img = document.createElement('img');
+        img.setAttribute('src', `${item?.poster}`);
+        showImg.appendChild(img);
+
+        const popDescription = document.createElement('div');
+        popDescription.classList.add('pop-description')
+        const ul = document.createElement('ul');
+        
+        const name = document.createElement('li');
+        const bold = document.createElement('strong');
+        bold.innerText = item?.prfnm;
+        name.appendChild(bold);
+        ul.appendChild(name);
+
+        const date = document.createElement('li');
+        date.innerText = `${item?.prfpdfrom} ~ ${item?.prfpdto}`;
+        ul.appendChild(date);
+
+        const genre = document.createElement('li');
+        genre.innerText = item?.genrenm;
+        ul.appendChild(genre);
+
+        popDescription.appendChild(ul);
+
+
+        popShow.appendChild(showImg);
+        popShow.appendChild(popDescription);
+
+        section.appendChild(popShow);
+      });
+    })
+  }catch(err) {
+    console.log(`Error: ${err}`);
+  }
+}
+
+const kidsShowApi = async ()=> {
+  try {
+    return fetch("http://localhost/api/kidsShowApi.php/")
+    .then(response => response.json())
+    .then(data => {
+      const section = document.querySelector('.three .pop');
+      section.innerHTML = "";
+      data.db.forEach(item => {
+        const popShow = document.createElement('div');
+        popShow.classList.add('pop-show');
+
+        const showImg = document.createElement('div');
+        showImg.classList.add('show-img');
+        
+        const img = document.createElement('img');
+        img.setAttribute('src', `${item?.poster}`);
+        showImg.appendChild(img);
+
+        const popDescription = document.createElement('div');
+        popDescription.classList.add('pop-description')
+        const ul = document.createElement('ul');
+        
+        const name = document.createElement('li');
+        const bold = document.createElement('strong');
+        bold.innerText = item?.prfnm;
+        name.appendChild(bold);
+        ul.appendChild(name);
+
+        const date = document.createElement('li');
+        date.innerText = `${item?.prfpdfrom} ~ ${item?.prfpdto}`;
+        ul.appendChild(date);
+
+        const genre = document.createElement('li');
+        genre.innerText = item?.genrenm;
+        ul.appendChild(genre);
+
+        popDescription.appendChild(ul);
+
+
+        popShow.appendChild(showImg);
+        popShow.appendChild(popDescription);
+
+        section.appendChild(popShow);
+      });
+    })
+  } catch(err) {
+    console.log(`Error: ${err}`);
+  }
+}
+
+
+
+
+boxOfficeApi();
+nowSowing();
+kidsShowApi();
