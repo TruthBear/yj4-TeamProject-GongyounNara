0622ba1e2cbad13517832b59ba8e1c5c0853d075
kwisdomkk <150204570+kwisdomkk@users.noreply.github.com>
2024-03-15 01:52:59 +0900

    signUp-error추가

---
+++ js/signUp.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const signUpForm = document.querySelector(".sign-up-form");
+  const passwordInput = document.querySelector(".password");
+  const passwordBox=document.querySelector(".password-box")
+  const password2Input = document.querySelector(".password2");
+  const password2Box = document.querySelector(".password-box2");
+  const idInput = document.querySelector(".id");
+  const idInputBox=document.querySelector(".id-box")
+  const emailInput = document.querySelector(".email");
+  const emailBox=document.querySelector(".email-box");
+  const nameInput = document.querySelector(".name");
+  const nameBox= document.querySelector(".name-box");
+  const userIdError = document.querySelector(".userid-error");
+  const userPwError = document.querySelector(".userpw-error");
+  const unmatchPwError = document.querySelector(".unmatch-pw");
+  const userMailError = document.querySelector(".usermail-error");
+  const userNickError = document.querySelector(".usernick-error");
+  signUpForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    if (passwordInput.value !== password2Input.value) {
+      unmatchPwError.style.display = "block";
+      password2Box.classList.add("error")
+      password2Input.classList.add("error-color")
+    } else {
+      unmatchPwError.style.display = "none";
+    }
+
+    if (idInput.value === "") {
+      userIdError.style.display = "block";
+      idInputBox.classList.add("error");
+      idInput.classList.add("error-color")
+    } else {
+      userIdError.style.display = "none";
+    }
+
+    if (passwordInput.value === "") {
+      userPwError.style.display = "block";
+      passwordBox.classList.add("error");
+      passwordInput.classList.add("error-color")
+    } else {
+      userPwError.style.display = "none";
+    }
+
+    if (password2Input.value === "") {
+      password2Box.classList.add("error");
+      password2Input.classList.add("error-color")
+    }
+
+    if (emailInput.value === "") {
+      userMailError.style.display = "block";
+      emailBox.classList.add("error");
+      emailInput.classList.add("error-color")
+    } else {
+      userMailError.style.display = "none";
+    }
+
+    if (nameInput.value === "") {
+      userNickError.style.display = "block";
+      nameBox.classList.add("error");
+      nameInput.classList.add("error-color")
+    } else {
+      userNickError.style.display = "none";
+    }
+
+  });
+});
+
+password2Input.addEventListener("input", function() {
+  if (passwordInput.value === password2Input.value) {
+      unmatchPwError.style.display = "none";
+      password2Box.classList.remove("error");
+      password2Input.classList.remove("error-color");
+  }
+});
--- signUp.php
+++ signUp.php
@@ -15,28 +15,42 @@
     <section class="sign-up">
       <h1>회원가입</h1>
       <form class="sign-up-form">
-        <div class="id-box">
-          <div class="icon"><i class="fa-regular fa-user"></i></div>
-          <input class="id" type="text" placeholder="아이디"/>
-          <button class="check-button">중복확인</button>
+        <div class="sign-up-input">
+          <div class="id-box">
+            <div class="icon"><i class="fa-regular fa-user"></i></div>
+            <input class="id" type="text" placeholder="아이디"/>
+            <button class="check-button">중복확인</button>
+          </div>
+          <div class="password-box">
+            <div class="icon"><i class="fa-solid fa-lock"></i></div>
+            <input class="password" type="password" placeholder="비밀번호"/>
+          </div>
+          <div class="password-box2">
+            <div class="icon"><i class="fa-solid fa-lock"></i></div>
+            <input class="password2" type="password" placeholder="비밀번호 확인"/>
+          </div>
+          <div class="email-box">
+            <div class="icon"><i class="fas fa-solid fa-envelope"></i></div>
+            <input class="email" type="email" placeholder="이메일"/>
+          </div>
+          <div class="name-box">
+            <div class="icon"><i class="fa-regular fa-user"></i></div>
+            <input class="name" type="text" placeholder="닉네임"/>
+          </div>
         </div>
-        <div class="password-box">
-          <div class="icon"><i class="fa-solid fa-lock"></i></div>
-          <input class="password" type="password" placeholder="비밀번호"/>
+        <!-- 에러 메세지 -->
+        <div class="userid-error">·아이디는 필수 정보입니다.</div>
+        <div class="userpw-error">·비밀번호는 필수 정보입니다.</div>
+        <div class="unmatch-pw">·비밀번호가 일치하지 않습니다.</div>
+        <div class="usermail-error">·이메일은 필수 정보입니다.</div>
+        <div class="usernick-error">·닉네임은 필수 정보입니다.</div>
+        <div class="button-box">
+          <button class="button">회원가입</button>
         </div>
-        <div class="name-box">
-          <div class="icon"><i class="fa-regular fa-user"></i></div>
-          <input class="name" type="text" placeholder="닉네임"/>
-        </div>
-        <div class="email-box">
-          <div class="icon"><i class="fas fa-solid fa-envelope"></i></div>
-          <input class="email" type="email" placeholder="이메일"/>
-        </div>
-        <button class="button">회원가입</button>
       </form>
     </section>
   </div>
-  
-    
+  <script src="./js/signUp.js"></script>
+  <script src="./js/signUpRemove.js"></script>
 </body>
 </html>
