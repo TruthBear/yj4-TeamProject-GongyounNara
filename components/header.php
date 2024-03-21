808e2f06cd1da64451f39a754172701330e457ca
jinyong <xmfkdnak98@gmail.com>
2024-03-21 16:47:41 +0900

    design: 이미지를 svg로 변경 및 호버시 색상 변경, 토글메뉴도 로그인과 비로그인시 보여주는 메뉴 변경

--- components/header.php
+++ components/header.php
@@ -2,36 +2,58 @@
 
 include "config/const.php";
 
-if (empty($_SESSION["user_id"])) {
+if(empty($_SESSION["user_id"])) {
   session_start();
 }
 
-if (isset($_SESSION["user_id"])) {
+if(isset($_SESSION["user_id"])) {
   echo "<script>console.log('아이디 있음')</script>";
   $user_id = $_SESSION["user_id"];
 } else {
   $user_id = "";
 }
 
-if (isset($_SESSION["user_name"])) {
+if(isset($_SESSION["user_name"])) {
   $user_name = $_SESSION["user_name"];
 } else {
   $user_name = "";
 }
 
-if (isset($_SESSION["user_profile"])) {
+if(isset($_SESSION['user_email'])) {
+  $user_email = $_SESSION['user_email'];
+} else {
+  $user_email = "";
+
+}
+
+
+if(isset($_SESSION["user_profile"])) {
   $user_profile = $_SESSION["user_profile"];
 } else {
   $user_profile = "";
 }
 
-if (isset($_SESSION["user_role"])) {
+if(isset($_SESSION["user_social_type"])) {
+  $user_social_type = $_SESSION["user_social_type"];
+} else {
+  $user_social_type = "";
+}
+
+if(isset($_SESSION["user_role"])) {
   $user_role = $_SESSION["user_role"];
 } else {
   $user_role = "";
 }
 
+echo "
+<script>
+console.log('$user_id, $user_name, $user_profile, $user_email, $user_social_type, $user_role');
+</script>
+";
+
 ?>
+
+
 
 <div class="head">
   <!-- 로고 및 메뉴 -->
@@ -53,7 +75,9 @@ if (isset($_SESSION["user_role"])) {
         <!-- area -->
         <div id="area-box">
           <div id="area-button">
-            <img src="assets/icons/Location Marker.svg" alt="Location Marker">
+          <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8.00008 0C3.58908 0 8.14446e-05 3.589 8.14446e-05 7.995C-0.0289186 14.44 7.69608 19.784 8.00008 20C8.00008 20 16.0291 14.44 16.0001 8C16.0001 3.589 12.4111 0 8.00008 0ZM8.00008 12C5.79008 12 4.00008 10.21 4.00008 8C4.00008 5.79 5.79008 4 8.00008 4C10.2101 4 12.0001 5.79 12.0001 8C12.0001 10.21 10.2101 12 8.00008 12Z" fill="white"/>
+          </svg>
             <div id="location">서울</div>
           </div>
           <ul class="option-list">
@@ -74,60 +98,113 @@ if (isset($_SESSION["user_role"])) {
         <div class='user-info'>
           <div><a href="signIn.php">로그인</a></div>
           <div><a href="signUp.php">회원가입</a></div>
-          <div class='user-img'>
-              <div class='user-btn'>
-                <img src="./assets/profiles/default.jpg" alt="">
-              </div>
-          </div>
         </div>
         <?php
           } else {
-            $logged = $user_name."님";
             ?>
-           <ul class="user-list">
-                <li class="user user-hidden" >
-                  <a href="#">
-                    <div>이름</div>
-                    <div>프로필 보기</div>
-                  </a>
-                </li>
-                <li class="user user-hidden" ><a href="#">관리자 페이지</a></li>
-                <li class="user user-hidden" ><a href="#">로그아웃</a></li>
-            </ul>
+            <div class='user-info'>
+              <div class='user-img'>
+                  <div class='user-btn'>
+                    <img src=<?=$user_profile?> alt="User Profile Image">
+                  </div>
+              </div>
+              <ul class="user-list">
+                  <li class="user user-hidden" >
+                    <a href="#">
+                      <div><?=$user_name?></div>
+                      <div>프로필 보기</div>
+                    </a>
+                  </li>
+                  <?php 
+                    if($user_role == "관리자") {
+                      echo '<li class="user user-hidden" ><a href="#">관리자 페이지</a></li>';
+                    } 
+                  ?>
+                  <?php 
+                  
+                  ?>
+                  <li class="user user-hidden" ><a href="./logout.php">로그아웃</a></li>
+              </ul>
+            </div>
         <?php
           }
           ?>
-        <div class = 'search-img'>
+        <!-- <div class = 'search-img'>
           <a href="./search.php"><img src="./assets/icons/SEARCH.png" alt=""></a>
+        </div> -->
+        <div class='search-img'>
+          <a href="./search.php">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" fill="#fff" viewBox="0 0 72 72">
+              <path d="M 31 11 C 19.973 11 11 19.973 11 31 C 11 42.027 19.973 51 31 51 C 34.974166 51 38.672385 49.821569 41.789062 47.814453 L 54.726562 60.751953 C 56.390563 62.415953 59.088953 62.415953 60.751953 60.751953 C 62.415953 59.087953 62.415953 56.390563 60.751953 54.726562 L 47.814453 41.789062 C 49.821569 38.672385 51 34.974166 51 31 C 51 19.973 42.027 11 31 11 z M 31 19 C 37.616 19 43 24.384 43 31 C 43 37.616 37.616 43 31 43 C 24.384 43 19 37.616 19 31 C 19 24.384 24.384 19 31 19 z"></path>
+            </svg>
+          </a>
         </div>
 
 </nav>
       <!-- 토글버튼 -->
       <div class="toggle">
           <div class="tog-btn">
-              <button id="toggleButton"><img src="../assets/icons/hambergerMenu-icon.png" alt=""></button>
+              <button id="toggleButton">
+              <svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M29.4737 19.7647C30.1225 19.765 30.7463 19.9745 31.2158 20.3498C31.6854 20.725 31.9647 21.2374 31.9958 21.7806C32.027 22.3238 31.8077 22.8562 31.3833 23.2676C30.9589 23.6789 30.362 23.9376 29.7162 23.9901L29.4737 24H2.52632C1.87753 23.9997 1.25374 23.7902 0.784191 23.415C0.314647 23.0397 0.0353482 22.5273 0.00416966 21.9841C-0.0270088 21.4409 0.192323 20.9085 0.616717 20.4971C1.04111 20.0858 1.63803 19.8271 2.28379 19.7746L2.52632 19.7647H29.4737ZM29.4737 9.88235C30.1437 9.88235 30.7863 10.1055 31.2601 10.5026C31.7338 10.8997 32 11.4384 32 12C32 12.5616 31.7338 13.1003 31.2601 13.4974C30.7863 13.8945 30.1437 14.1176 29.4737 14.1176H2.52632C1.8563 14.1176 1.21372 13.8945 0.739941 13.4974C0.266165 13.1003 0 12.5616 0 12C0 11.4384 0.266165 10.8997 0.739941 10.5026C1.21372 10.1055 1.8563 9.88235 2.52632 9.88235H29.4737ZM29.4737 0C30.1437 0 30.7863 0.223109 31.2601 0.620245C31.7338 1.01738 32 1.55601 32 2.11765C32 2.67928 31.7338 3.21791 31.2601 3.61505C30.7863 4.01219 30.1437 4.23529 29.4737 4.23529H2.52632C1.8563 4.23529 1.21372 4.01219 0.739941 3.61505C0.266165 3.21791 0 2.67928 0 2.11765C0 1.55601 0.266165 1.01738 0.739941 0.620245C1.21372 0.223109 1.8563 0 2.52632 0H29.4737Z" fill="white"/>
+              </svg>
+
+              </button>
           </div>
           <div class="tog-menu">
               <div class="id-box">
+                <?php 
+                  if(!$user_id) {
+                ?>
                   <ul class="id">
-                      <li>🔴 로그인하세요.</li>
+                      <li><a href="signIn.php">로그인해주세요.</a></li>
                       <li><button id="closeButton"><img src="../assets/icons/bambergerCancel-icon.png" alt="토글 메뉴 끄기"></button></li>
                   </ul>
+                <?php
+                  } else {
+                ?>
+                  <ul class="id">
+                    <li class="toggle-user-wrap">
+                      <div class="toggle-user-img-wrap">
+                        <img src=<?=$user_profile?> alt="User Profile Image">
+                      </div>
+                      <span><strong><?=$user_name?></strong></span>
+                    </li>
+                    <li><button id="closeButton"><img src="../assets/icons/bambergerCancel-icon.png" alt="토글 메뉴 끄기"></button></li>
+                  </ul>
+                <?php
+                  }
+                ?>
               </div>
               <div class="menus">
                   <div>
                       <ul class="menu">
                         <li><a href="./search.php">공연검색</a></li>
-                          <li><a href="./list.php">공연목록</a></li>
-                          <li><a href="">메뉴1</a></li>
-                          <li><a href="">메뉴1</a></li>
+                        <li><a href="./list.php">공연목록</a></li>
+                        <li><a href="#">메뉴1</a></li>
+                        <li><a href="#">메뉴1</a></li>
                       </ul>
                   </div>
                   <div>
-                          <ul class="as">
-                              <li>관리자 페이지</li>
-                              <li>로그아웃</li>
-                          </ul>
+                    <?php 
+                      if(!$user_id) {
+                    ?>
+                      <ul class="as">
+                        <li><a href="signIn.php">로그인</a></li>
+                        <li><a href="signUp.php">회원가입</a></li>
+                      </ul>
+                    <?php 
+                      } else {
+                    ?>
+                      <ul class="as">
+                        <?php if($user_role == "관리자") {
+                          echo '<li><a href="#">관리자 페이지</a></li>';
+                        } ?>
+                          <li><a href="logout.php">로그아웃</a></li>
+                      </ul>
+                    <?php
+                    }
+                    ?>
                   </div>
               </div>
           </div>
