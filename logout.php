e351bfc5ed9eb4d1313eef085260dcf60fed62e7
Jin young <xmfkdnak98@gmail.com>
2024-03-22 02:33:23 +0900

    소셜로그아웃 네이버 카카오 추가

--- logout.php
+++ logout.php
@@ -1,6 +1,20 @@
 <?php
 
+include "controller/social_logout_controller.php";
+
 session_start();
+
+$access_token = $_SESSION['access_token'];
+$user_social_type = $_SESSION['user_social_type'];
+$logout = new logout_controller($access_token);
+
+if($user_social_type == "kakao") {
+  $logout->kakao_logout();
+} else if($user_social_type == "naver") {
+  $logout->naver_logout();
+}
+
+
 unset($_SESSION['user_id']);
 unset($_SESSION['user_name']);
 unset($_SESSION['user_email']);
